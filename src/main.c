a9970b3d13fd81859aafe953004d594c664d0bf3
akudlacek <7029639+akudlacek@users.noreply.github.com>
2019-05-10 12:29:23 -0500

    Added more examples
    Signed-off-by: akudlacek <7029639+akudlacek@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -19,8 +19,11 @@
 *************************************************^************************************************/
 static void void_func_ex(void);
 static void int_func_ex(int arg);
+static void uint8_func_ex(uint8_t arg);
+static void ulint_func_ex(unsigned long arg);
 static void float_func_ex(float arg);
 static void str_func_ex(const char *arg);
+static void duck_func_ex(void);
 
 static void cmd_line_init(void);
 
@@ -33,16 +36,27 @@ void tx_string_wrapper(const char *string_ptr);
 *************************************************^************************************************/
 char tmp_str[100];
 
+
+/* Available function types
+	CLI_VOID,
+	CLI_INT,
+	CLI_UINT8,
+	CLI_ULINT,
+	CLI_FLOAT,
+	CLI_STRING
+*/
 static const cli_command_t cli_cmd_list[] =
 {
 	//cmd name, fptr, help string
 	CLI_HELP_CMD_LIST_ENTRY,
 
-	{"help",  CLI_VOID_FPTR(cli_help_command),  HELP("Lists the commands available")},
 	{"void",  CLI_VOID_FPTR(void_func_ex),      HELP("void just runs the function")},
 	{"int",   CLI_INT_FPTR(int_func_ex),        HELP("e.g. int -4")},
+	{"uint8", CLI_UINT8_FPTR(uint8_func_ex),    HELP("e.g. uint8_t 200")},
+	{"ulint", CLI_ULINT_FPTR(ulint_func_ex),    HELP("e.g. unsigned long 4294967295")},
 	{"float", CLI_FLOAT_FPTR(float_func_ex),    HELP("e.g. float 9.87654")},
 	{"str",   CLI_STRING_FPTR(str_func_ex),     HELP("type str followed by your string")},
+	{"duck",   CLI_STRING_FPTR(duck_func_ex),   HELP("prints a duck")},
 
 	CLI_CMD_LIST_END                                             // must be LAST
 };
@@ -98,7 +112,31 @@ void void_func_ex(void)
 static void int_func_ex(int arg)
 {
 	cli_print("int_func_ex was entered\r\n");
-	snprintf(tmp_str, sizeof(tmp_str), "you put int: %d\r\n", arg);
+	snprintf(tmp_str, sizeof(tmp_str), "you put int: %i\r\n", arg);
+	cli_print(tmp_str);
+}
+
+/******************************************************************************
+*  \brief
+*
+*  \note
+******************************************************************************/
+static void uint8_func_ex(uint8_t arg)
+{
+	cli_print("uint8_func_ex was entered\r\n");
+	snprintf(tmp_str, sizeof(tmp_str), "you put uint8_t: %u\r\n", arg);
+	cli_print(tmp_str);
+}
+
+/******************************************************************************
+*  \brief
+*
+*  \note
+******************************************************************************/
+static void ulint_func_ex(unsigned long arg)
+{
+	cli_print("ulint_func_ex was entered\r\n");
+	snprintf(tmp_str, sizeof(tmp_str), "you put unsigned long: %lu\r\n", arg);
 	cli_print(tmp_str);
 }
 
@@ -125,6 +163,32 @@ static void str_func_ex(const char *arg)
 	snprintf(tmp_str, sizeof(tmp_str), "you put string: %s\r\n", arg);
 	cli_print(arg);
 	cli_print("\r\n");
+}
+
+/******************************************************************************
+*  \brief
+*
+*  \note
+******************************************************************************/
+static void duck_func_ex(void)
+{
+	cli_print(
+				"        ,----,\r\n"
+				"   ___.`      `,\r\n"
+				"   `===  D     :\r\n"
+				"     `'.      .'\r\n"
+				"        )    (                   ,\r\n"
+				"       /      \\_________________/|\r\n"
+				"      /                          |\r\n"
+				"     |                           ;\r\n"
+				"     |               _____       /\r\n"
+				"     |      \\       ______7    ,'\r\n"
+				"     |       \\    ______7     /\r\n"
+				"      \\       `-,____7      ,'   jgs\r\n"
+				"^~^~^~^`\\                  /~^~^~^~^\r\n"
+				"  ~^~^~^ `----------------' ~^~^~^\r\n"
+				" ~^~^~^~^~^^~^~^~^~^~^~^~^~^~^~^~\r\n"
+			);
 }
 
 /******************************************************************************
